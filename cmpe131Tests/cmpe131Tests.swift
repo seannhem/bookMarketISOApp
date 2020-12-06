38a3e1436470ad41fc6b2063f322d8ad5849eacd
Nemanja Rajic <nrajic30@gmail.com>
2020-12-05 19:34:36 -0800

    Initial Commit

---
+++ cmpe131Tests/cmpe131Tests.swift
@@ -0,0 +1,33 @@
+//
+//  cmpe131Tests.swift
+//  cmpe131Tests
+//
+//  Created by Nemanja Rajic on 12/5/20.
+//
+
+import XCTest
+@testable import cmpe131
+
+class cmpe131Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
